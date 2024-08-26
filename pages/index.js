b80b3cccf45cde92526fdc2b2dbf1af24ef38c1c
borsos-ilo <ilona.kedracka@wpengine.com>
2024-08-26 09:49:34 +0200

    subtitle change

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       >
       <div className="text-center max-w-3xl mx-auto">
         <h1 className="text-4xl font-bold font-heading mb-4">Mój cyfrowy ogród</h1>
-        <p className="text-xl mb-8 font-body">To miejsce to mój cyfrowy strumień świadomości. Płynie on swobodnie, meandrując między różnymi tematami. Czasem spokojny i przejrzysty, innym razem wzburzony i mętny.</p>
+        <p className="text-xl mb-8 font-body">To miejsce to mój cyfrowy strumień świadomości. Płynie on dość swobodnie, meandrując między różnymi tematami, które mnie interesują.</p>
         
         <section className="mb-12">
           <p className="text-lg mb-4 font-body">
