827bad778cb5552bdd7e527a67b27113da7e853c
borsos-ilo <ilona.kedracka@wpengine.com>
2024-08-27 17:42:45 +0200

    cdn cache control

--- pages/api/purgebypath.js
+++ pages/api/purgebypath.js
@@ -1,8 +1,10 @@
 import { purgePaths } from '@wpengine/edge-cache';
 
-try {
-  const paths = ['new-page']
-  await purgePaths(paths)
-} catch (error) {
-  console.error(error)
-}
+export default async function handler(req, res) { 
+    try {
+        const paths = ['new-page']
+        await purgePaths(paths)
+      } catch (error) {
+        console.error(error)
+      }
+  }
