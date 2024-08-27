5d5c8d6a2fbbbcdabeeef7fbc38ae954e4862c73
borsos-ilo <ilona.kedracka@wpengine.com>
2024-08-27 09:05:47 +0200

    remove reference to atlas-next

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import { withAtlasConfig } from "@wpengine/atlas-next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withAtlasConfig(nextConfig);
+export default nextConfig;
