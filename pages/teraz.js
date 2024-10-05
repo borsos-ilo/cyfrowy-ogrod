4d362edb5c011ce1c87d09c333a45c433673a34c
borsos-ilo <ilona.kedracka@wpengine.com>
2024-10-06 00:37:47 +0200

    kompletna strona teraz

--- pages/teraz.js
+++ pages/teraz.js
@@ -1,55 +1,88 @@
 import Layout from "@/components/Layout";
 import RandomColoredLinksContent from "@/components/RandomColoredLinksContent";
+import Image from "next/image";
 
-export default function Now ({pageData}) {
-    console.log(JSON.stringify(pageData))
-    const {title, content} = pageData[0].node;
-    // const {books, music, moviesSeries, projects} = update
-    return (
-        <Layout title={title}>
-            <div className="max-w-4xl mx-auto">
-                <div className="flex flex-col md:flex-row gap-8">
-                <RandomColoredLinksContent content={content} />
-                </div>
+export default function Now({ pageData }) {
+  const { title, content, update, featuredImage } = pageData[0].node;
+  return (
+    <Layout title={title}>
+      <div className="max-w-4xl">
+        <h1 className="text-4xl font-bold mb-6 text-center font-heading">{title}</h1>
+        <div className="flex flex-wrap -mx-3">
+          <div className="basis-3/5 md:w-1/2 px-3 mb-6 ">
+              <RandomColoredLinksContent content={content} />
+          </div>
+          <div className="basis-2/5 md:w-1/2 px-3 mb-6 flex items-center justify-center">
+              <div className="w-full">
+                <Image
+                  src={featuredImage.node.sourceUrl}
+                  alt={featuredImage.node.altText || title}
+                  width={400}
+                  height={400}
+                  layout="responsive"
+                  className="rounded-lg"
+                />
             </div>
-        </Layout>
-    )
-
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6">
+              <h2 className="text-2xl font-semibold mb-4 font-heading">Muzyka</h2>
+              <RandomColoredLinksContent content={update.music} />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6">
+              <h2 className="text-2xl font-semibold mb-4 font-heading">Książki</h2>
+              <RandomColoredLinksContent content={update.books} />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6">
+              <h2 className="text-2xl font-semibold mb-4 font-heading">Filmy & seriale</h2>
+              <RandomColoredLinksContent content={update.moviesSeries} />
+          </div>
+          <div className="w-full md:w-1/2 px-3 mb-6">
+              <h2 className="text-2xl font-semibold mb-4 font-heading">Projekty</h2>
+              <RandomColoredLinksContent content={update.projects} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-
-
 export async function getStaticProps() {
-    const response = await fetch(`${process.env.WORDPRESS_API_URL}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        query: `
+  const response = await fetch(`${process.env.WORDPRESS_API_URL}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `
         query GetNow {
-            update(first: 1) {
-                edges {
-                    node {
-                        title
-                        content
-                        update {
-                            books
-                            music
-                            moviesSeries
-                            projects
-                            }
-                    }
+          update(first: 1) {
+            edges {
+              node {
+                title
+                content
+                update {
+                  books
+                  music
+                  moviesSeries
+                  projects
                 }
+                featuredImage {
+                  node {
+                    altText
+                    sourceUrl
+                  }
+                }
+              }
             }
-            }
-        `
-      })
-    });
-  
-    const { data } = await response.json();
-  
-    return {
-      props: {
-        pageData: data.update.edges
-      }
-    }
-  }
+          }
+        }
+        `,
+    }),
+  });
+
+  const { data } = await response.json();
+
+  return {
+    props: {
+      pageData: data.update.edges,
+    },
+  };
+}
