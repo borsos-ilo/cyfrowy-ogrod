c32b668b0449814b1edbfe7ad5c56394f6285bc9
borsos-ilo <ilona.kedracka@wpengine.com>
2024-10-06 11:29:29 +0200

    playing with image size for social cards

--- components/Layout.js
+++ components/Layout.js
@@ -30,6 +30,8 @@ export default function Layout({
         <meta property="og:type" content="website" />
         <meta property="og:url" content={fullUrl} />
         <meta property="og:image" content={ogImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
