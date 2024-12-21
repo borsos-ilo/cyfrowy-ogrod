e8d67d2d93592e2931ad09b6464c72baa2010add
borsos-ilo <ilona.kedracka@wpengine.com>
2024-12-22 00:23:09 +0100

    adding google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -6,26 +6,23 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Inconsolata&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
-        {/* GTM script */}
+        {/* Google Analytics */}
+        <script 
+          async 
+          src="https://www.googletagmanager.com/gtag/js?id=G-5SRHFCRBSY"
+        />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TM487DDW');`
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5SRHFCRBSY');
+            `
           }}
         />
       </Head>
       <body>
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TM487DDW"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Main />
         <NextScript />
       </body>
