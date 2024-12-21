8c88d8ae54a8ef1d04d006b5b499eb3ce7402ff6
borsos-ilo <ilona.kedracka@wpengine.com>
2024-12-22 00:12:36 +0100

    adding google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,27 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Inconsolata&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
+        {/* GTM script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TM487DDW');`
+          }}
+        />
       </Head>
       <body>
-        <Main/>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TM487DDW"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <Main />
         <NextScript />
       </body>
     </Html>
